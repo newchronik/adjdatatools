44badb5b1fc4707f93d0263f8e2ad5f4d095751c
Anton <anton.kvasnitsa@gmail.com>
2020-12-18 09:06:38 +0200

    Small update

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='smart-data-tools',
     version='0.3.0',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/newchronik/smart-data-tools',
     license='MIT',
+    keywords='data preprocessing scaler',
     author='Anton Kvasnitsa',
     author_email='anton.kvasnitsa@gmail.com',
-    description='This library contains adjusted tools for data preprocessing and working with mixed data types.'
+    description='This library contains adjusted tools for data preprocessing and working with mixed data types.',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'math'
+    ],
+    include_package_data=True,
 )
