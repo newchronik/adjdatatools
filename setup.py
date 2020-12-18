e407c1b193192ec5b23908f00387313793d1e058
Anton <anton.kvasnitsa@gmail.com>
2020-12-18 22:40:25 +0200

    Change directories

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adjdatatools',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     url='https://github.com/newchronik/adjdatatools',
     license='MIT',
