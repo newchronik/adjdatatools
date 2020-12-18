b08d07aa6221a6550726a69b4c8ada6129c7aeff
Anton <anton.kvasnitsa@gmail.com>
2020-12-18 11:35:23 +0200

    Small update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smart-data-tools',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/newchronik/smart-data-tools',
     license='MIT',
@@ -12,8 +12,7 @@ setup(
     description='This library contains adjusted tools for data preprocessing and working with mixed data types.',
     install_requires=[
         'numpy',
-        'pandas',
-        'math'
+        'pandas'
     ],
     include_package_data=True,
 )
